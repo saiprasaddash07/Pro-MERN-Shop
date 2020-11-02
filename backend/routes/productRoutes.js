19d56de1759cc9d65b2c78142fc07e8eadf62609
Sai Prasad Dash <sabitadash007@gmail.com>
2020-11-02 19:42:24 +0530

    Admin functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getProducts,getProductById} = require('../controllers/productController');
+const {getProducts,getProductById,deleteProduct,updateProduct,createProduct} = require('../controllers/productController');
+const protect = require('../middleware/authMiddleWare');
+const admin = require('../middleware/adminMiddleware');
 
 // router.get('/',product.getProducts);
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect,admin,createProduct);
 
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect,admin,deleteProduct).put(protect,admin,updateProduct);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const colors = require('colors');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
 const {notFound,errorHandler} = require('./middleware/errorMiddleware');
 
 dotenv.config();
@@ -22,10 +24,14 @@ app.get('/',(req,res)=>{
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
+app.use('/api/upload',uploadRoutes);
 
 app.get('/api/config/paypal',(req,res) => {
     res.send(process.env.PAYPAL_CLIENT_ID);
 })
+
+//const __dirname = path.resolve();
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
