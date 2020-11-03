ff80d4fe94bd8c2985ac4d08eb1e8b6f80a60fda
Sai Prasad Dash <sabitadash007@gmail.com>
2020-11-03 19:44:14 +0530

    Prepared for deployment in heroku

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Hello');
-});
-
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
@@ -35,8 +31,16 @@ app.get('/api/config/paypal',(req,res) => {
     res.send(process.env.PAYPAL_CLIENT_ID);
 })
 
-//const __dirname = path.resolve();
-//app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*',(req,res)=>
+        res.sendFile(path.resolve(__dirname,'..', 'frontend', 'build', 'index.html'))
+    );
+}else{
+    app.get('/',(req,res)=>{
+        res.send('API is Running');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/components/productCarousel.js
+++ frontend/src/components/productCarousel.js
@@ -24,7 +24,7 @@ class ProductCarousel extends Component {
                                 {this.props.productTopRated.products.map(product => (
                                     <Carousel.Item key={product._id}>
                                         <Link to={`/product/${product._id}`}>
-                                            <Image src={product.image} alt={product.name} fluid></Image>
+                                            <Image src={product.image} alt={product.name} fluid />
                                             <Carousel.Caption className="carousel-caption">
                                                 <h2>
                                                     {product.name} (${product.price})
